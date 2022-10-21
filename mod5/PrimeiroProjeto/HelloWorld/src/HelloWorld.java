e408612a0ed096f7fb0bec38d270fe47b3dbd69b
Herique <heriquevitor3@gmail.com>
2022-10-21 01:44:39 -0300

    Work module 5

---
+++ mod5/PrimeiroProjeto/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void apresentar(){
+        System.out.println("Hello World, I'm Hérique, \nI'm from Brazil and it's fantastic to be here, learning Java.");
+    }
+}
